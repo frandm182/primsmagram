452736c57efd6379afe9990471c5b1751edc88fc
FRANCISCO JAVIER DIAZ MARTIN <franciscojavier.diaz.martin.contractor@bbva.com>
2021-01-13 16:16:52 +0100

    refactor fullPost with fragments

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,16 +1,12 @@
 import { prisma } from '../../../../generated/prisma-client';
-import { COMMENT_FRAGMEMNT } from '../../../fragments';
+import { FULL_POST_FRAGMEMNT } from '../../../fragments';
 
 export default {
   Query: {
-    seeFullPost: async(_, args) => {
+    seeFullPost: (_, args) => {
       const { id } = args;
-      const post = await prisma.post({ id });
-      const comments = await prisma.post({ id }).comments().$fragment(COMMENT_FRAGMEMNT);
-      const files = await prisma.post({ id }).files();
-      const user = await prisma.post({ id }).user();
-
-      return { post, comments, files, user };
+      console.log(id)
+      return prisma.post({ id }).$fragment(FULL_POST_FRAGMEMNT);
     }
   }
 }
--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,34 @@
+export const USER_FRAGMEMNT = `
+  id
+  userName
+`;
+
 export const COMMENT_FRAGMEMNT = `
-  fragment CommentParts on Comment {
+  id
+  text
+  user {
+    ${USER_FRAGMEMNT}
+  }
+`;
+
+export const FILE_FRAGMEMNT = `
+  id
+  url
+`;
+
+export const FULL_POST_FRAGMEMNT = `
+  fragment PostParts on Post {
     id
-    text
+    location
+    caption
     user {
-      userName
+      ${USER_FRAGMEMNT}
+    }
+    files {
+      ${FILE_FRAGMEMNT}
+    }
+    comments {
+      ${COMMENT_FRAGMEMNT}
     }
   }
 `;
